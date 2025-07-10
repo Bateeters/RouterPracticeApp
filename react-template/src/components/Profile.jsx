8d5599dd9de31f55c1d862583a7b192a81045495
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-10 16:02:01 -0400

    Added routing to home and interior page

---
+++ react-template/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+        <div>
+            <h1>Hello from profile page!</h1>
+            <p>So, how are you?</p>
+            <a href="/">back to home</a>
+        </div>
+    );
+};
+
+export default Profile;
