a5325fcd0c74191786453f568bd514c12a563a11
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-13 11:10:40 -0400

    Updated Profile.jsx with Outlet

--- react-template/src/components/Profile.jsx
+++ react-template/src/components/Profile.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Profile = () => {
     return (
         <div>
             <h1>Hello from profile page!</h1>
             <p>So, how are you?</p>
-            <Link to="/">back to home</Link>
+            <hr/>
+            <h2>The profile visited is here:</h2>
+            <Outlet />
         </div>
     );
 };
