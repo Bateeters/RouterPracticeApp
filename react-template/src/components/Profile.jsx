3403d5b6f400c1e45f43df6c3ee6129b12ab214d
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-13 11:01:42 -0400

    Set up initial React Router

--- react-template/src/components/Profile.jsx
+++ react-template/src/components/Profile.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Profile = () => {
     return (
         <div>
             <h1>Hello from profile page!</h1>
             <p>So, how are you?</p>
-            <a href="/">back to home</a>
+            <Link to="/">back to home</Link>
         </div>
     );
 };
