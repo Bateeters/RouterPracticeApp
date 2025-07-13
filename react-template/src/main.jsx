0820565b790c4c391f810dae7d0e1934bc81a0bb
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-13 11:08:17 -0400

    Added spinach and popeye children routes

--- react-template/src/main.jsx
+++ react-template/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
+import Popeye from "./components/Popeye";
+import Spinach from "./components/Spinach";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "profile",
     element: <Profile />,
+    children: [
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ]
   },
 ]);
 
